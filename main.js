a094066c3db451a8e07c819e549b1c45080c4cff
lubotn <142837650+lubotn@users.noreply.github.com>
2024-09-23 11:45:04 -0300

    ihu8

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 // let nomeUsuario='Lucas, Gabriel';
 // alert (nomeUsuario);
-function areaquadrado(lado){
-    return lado**;
-}
-console.log(areaquadrado(5));
-function soma(num1, num2){
-    return num1 + num2 ;
+function soma(num1 , num2){
+    return num1 + num2;
 }
 console.log(soma(5,5));
+
+function areaquadrado(lado){
+    return lado **2;
+}
+console.log(areaquadrado(5));
+
+
