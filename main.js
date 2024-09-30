f3b04667886b031404a70ec33d357b55e7f6aa8d
lubotn <142837650+lubotn@users.noreply.github.com>
2024-09-30 11:16:33 -0300

    bbb

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-// let nomeUsuario='Lucas, Gabriel';
-// alert (nomeUsuario);
-function soma(num1 , num2){
-    return num1 + num2;
-}
-console.log(soma(5,5));
+// // let nomeUsuario='Lucas, Gabriel';
+// // alert (nomeUsuario);
+// function soma(num1 , num2){
+//     return num1 + num2;
+// }
+// console.log(soma(5,5));
 
-function areaquadrado(lado){
-    return lado **2;
-}
-console.log(areaquadrado(5));
-let areatriangulo= function(base, altura, ){
-    return base * altura /2}
-console.log(areatriangulo(732,533,2))
+// function areaquadrado(lado){
+//     return lado **2;
+// }
+// console.log(areaquadrado(5));
+// let areatriangulo= function(base, altura, ){
+//     return base * altura /2}
+// console.log(areatriangulo(732,533,2))
 
-let a = parseInt (prompt("primeiro valor"))
-let b = parseInt (prompt("segundo valor"))
-let c = parseInt (prompt("terceiro valor"))
-function media (a,b,c){
-    return ((a+b+c)/3);
-}
-document.write(media(a, b, c))
+// let a = parseInt (prompt("primeiro valor"))
+// let b = parseInt (prompt("segundo valor"))
+// let c = parseInt (prompt("terceiro valor"))
+// function media (a,b,c){
+//     return ((a+b+c)/3);
+// }
+// document.write(media(a, b, c))
+
+let estados=['acre','amapa','amazonas','para','rondonia','tocantins','alagoas','bahia','ceara','maranhao','paraiba','pernambuco','piaui','rio grande do norte','sergipe','distrito federal','goais','mato grosso','mato grosso do sul','espirito santo','minas gerais','rio de janeiro','sao paulo']
